5c028c99dd3a2bf843a0b30a692a92a7ad7d573f
Jeffy <jfrancis@tndm.de>
2014-06-20 15:21:21 +0200

    added views

--- films/views.py
+++ films/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+
+from films.models import Film
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    film_list = Film.objects.all()
+    template = loader.get_template('films/index.html')
+    context = RequestContext(request,{
+        'film_list': film_list,
+    })
+    return HttpResponse(template.render(context))
